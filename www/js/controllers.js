d82459a6f83e7e36d939036b2c309f3c568eaf3c
Albert Arvesu <albertarvesu@gmail.com>
2015-07-23 16:18:25 +0800

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+	$scope.pair = function(){
+
+	};
+
+	$scope.scan = function(){
+
+	};
+
+	$scope.sync = function(){
+
+	};
+
+	$scope.unpair = function(){
+
+	};
+})
+
+.controller('BlankCtrl', function($scope) {
+})
